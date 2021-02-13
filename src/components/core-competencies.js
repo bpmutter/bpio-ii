6a4c50e7871f88ae5f03547216abb61b941386c3
Ben Perlmutter <benperlmutter94@gmail.com>
2021-02-13 10:14:21 -0500

    shuffle core competencies

--- src/components/core-competencies.js
+++ src/components/core-competencies.js
@@ -4,11 +4,30 @@ import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
 import styles from './core-competencies.module.css'
 
+function shuffle(array) {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array
+}
 function CoreCompetencies({ skills }) {
+  shuffle(skills) //why not
+
   return (
     <Section title="Core Competencies">
       <div className={styles.skills}>
-        {skills.reverse().map((skill) => (
+        {skills.map((skill) => (
           <div key={skill.node.name}>
             <Img alt={skill.node.name} fixed={skill.node.media.fixed} />
             <h4 className={styles.title}>{skill.node.name}</h4>
