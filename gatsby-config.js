3977994decc6073a4f4b9a1b8f42f8a26759be57
Ben Perlmutter <57849986+bpmutter@users.noreply.github.com>
2023-11-11 11:04:55 -0500

    Update gatsby-config.js
    Update metadata from template default

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Contentful starter',
+    title: 'ben.perlmutter.io',
   },
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
