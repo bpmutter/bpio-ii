4ed97950601836d7775dab216687e5d7730beea6
Ben Perlmutter <mongodben@mongodb.com>
2025-06-08 16:47:46 -0400

    new approach

--- src/constants.ts
+++ src/constants.ts
@@ -1,111 +1,112 @@
-import IconMail from "@/assets/icons/IconMail.svg";
-import IconGitHub from "@/assets/icons/IconGitHub.svg";
-import IconBrandX from "@/assets/icons/IconBrandX.svg";
-import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
-import IconFacebook from "@/assets/icons/IconFacebook.svg";
-import IconTelegram from "@/assets/icons/IconTelegram.svg";
-import IconPinterest from "@/assets/icons/IconPinterest.svg";
-import RobotImage from "@/assets/images/robot.ghibli.png";
-import CloudImage from "@/assets/images/sitting_on_cloud.ghibli.png";
-import TechnicalWriting from "@/assets/images/technical_writing.ghibli.png";
-import ReadingImage from "@/assets/images/reading.ghibli.png";
-import { SITE } from "@/config";
-import type { ImageMetadata } from "astro";
+import IconMail from '@/assets/icons/IconMail.svg'
+import IconGitHub from '@/assets/icons/IconGitHub.svg'
+import IconBrandX from '@/assets/icons/IconBrandX.svg'
+import IconLinkedin from '@/assets/icons/IconLinkedin.svg'
+import IconWhatsapp from '@/assets/icons/IconWhatsapp.svg'
+import IconFacebook from '@/assets/icons/IconFacebook.svg'
+import IconTelegram from '@/assets/icons/IconTelegram.svg'
+import IconPinterest from '@/assets/icons/IconPinterest.svg'
+import RobotImage from '@/assets/images/robot.ghibli.png'
+import CloudImage from '@/assets/images/sitting_on_cloud.ghibli.png'
+import TechnicalWriting from '@/assets/images/technical_writing.ghibli.png'
+import ReadingImage from '@/assets/images/reading.ghibli.png'
+import { SITE } from '@/config'
+import type { ImageMetadata } from 'astro'
+import type { AstroComponentFactory } from 'astro/runtime/server/index.js'
 
 interface IconLink {
-  name: string;
-  href: string;
-  linkTitle: string;
-  icon: ImageMetadata;
+  name: string
+  href: string
+  linkTitle: string
+  icon: AstroComponentFactory
 }
 
 export const SOCIALS: IconLink[] = [
   {
-    name: "Github",
-    href: "https://github.com/bpmutter",
+    name: 'Github',
+    href: 'https://github.com/bpmutter',
     linkTitle: ` ${SITE.title} on Github`,
     icon: IconGitHub,
   },
   {
-    name: "X",
-    href: "https://x.com/bpmutter",
+    name: 'X',
+    href: 'https://x.com/bpmutter',
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
-    name: "LinkedIn",
-    href: "https://www.linkedin.com/in/ben-perlmutter/",
+    name: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/ben-perlmutter/',
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
-    name: "Mail",
-    href: "mailto:ben@perlmutter.io",
+    name: 'Mail',
+    href: 'mailto:ben@perlmutter.io',
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
-] as const;
+] as const
 
 export const SHARE_LINKS: IconLink[] = [
   {
-    name: "WhatsApp",
-    href: "https://wa.me/?text=",
+    name: 'WhatsApp',
+    href: 'https://wa.me/?text=',
     linkTitle: `Share this post via WhatsApp`,
     icon: IconWhatsapp,
   },
   {
-    name: "Facebook",
-    href: "https://www.facebook.com/sharer.php?u=",
+    name: 'Facebook',
+    href: 'https://www.facebook.com/sharer.php?u=',
     linkTitle: `Share this post on Facebook`,
     icon: IconFacebook,
   },
   {
-    name: "X",
-    href: "https://x.com/intent/post?url=",
+    name: 'X',
+    href: 'https://x.com/intent/post?url=',
     linkTitle: `Share this post on X`,
     icon: IconBrandX,
   },
   {
-    name: "Telegram",
-    href: "https://t.me/share/url?url=",
+    name: 'Telegram',
+    href: 'https://t.me/share/url?url=',
     linkTitle: `Share this post via Telegram`,
     icon: IconTelegram,
   },
   {
-    name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
+    name: 'Pinterest',
+    href: 'https://pinterest.com/pin/create/button/?url=',
     linkTitle: `Share this post on Pinterest`,
     icon: IconPinterest,
   },
   {
-    name: "Mail",
-    href: "mailto:?subject=See%20this%20post&body=",
+    name: 'Mail',
+    href: 'mailto:?subject=See%20this%20post&body=',
     linkTitle: `Share this post via email`,
     icon: IconMail,
   },
-] as const;
+] as const
 
 interface CoreCompetency {
-  name: string;
-  image: ImageMetadata;
+  name: string
+  image: ImageMetadata
 }
 
 export const CORE_COMPETENCIES: CoreCompetency[] = [
   {
-    name: "Learning Quickly",
+    name: 'Learning Quickly',
     image: ReadingImage,
   },
   {
-    name: "Cloud Computing",
+    name: 'Cloud Computing',
     image: CloudImage,
   },
   {
-    name: "Generative AI",
+    name: 'Generative AI',
     image: RobotImage,
   },
   {
-    name: "Technical Writing",
+    name: 'Technical Writing',
     image: TechnicalWriting,
   },
-];
+]
