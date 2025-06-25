e9fdd827c575b2d5e978e0bbf2b8b50321a65d5a
Ben Perlmutter <mongodben@mongodb.com>
2025-06-24 20:58:18 -0400

    fix edit link

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const SITE = {
-  website: "https://ben.perlmutter.io/", // replace this with your deployed domain
-  author: "Ben Perlmutter",
-  profile: "https://ben.perlmutter.io/",
+  website: 'https://ben.perlmutter.io/', // replace this with your deployed domain
+  author: 'Ben Perlmutter',
+  profile: 'https://ben.perlmutter.io/',
   desc: "Ben Perlmutter's personal website.",
-  title: "Ben Perlmutter",
-  ogImage: "astropaper-og.jpg",
+  title: 'Ben Perlmutter',
+  ogImage: 'astropaper-og.jpg',
   lightAndDarkMode: true,
   postPerIndex: 5,
   postPerPage: 10,
@@ -13,10 +13,10 @@ export const SITE = {
   showBackButton: true, // show back button in post detail
   editPost: {
     enabled: true,
-    text: "Suggest Changes",
-    url: "https://github.com/bpmutter/bpio-iii/edit/main/",
+    text: 'Suggest Changes',
+    url: 'https://github.com/bpmutter/bpio-ii/edit/master/',
   },
   dynamicOgImage: true,
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "America/New_York", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-} as const;
+  lang: 'en', // html lang code. Set this empty and default will be "en"
+  timezone: 'America/New_York', // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const
